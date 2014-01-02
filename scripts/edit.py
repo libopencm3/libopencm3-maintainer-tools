97b47c9000ecf085d15c0794b53c8475620addb3
Frantisek Burian <BuFran@seznam.cz>
2014-01-02 17:31:21 +0100

    Almost final version

--- scripts/edit.py
+++ scripts/edit.py
@@ -26,6 +26,121 @@ from optparse import OptionParser, OptionGroup
 
 global options
 
+
+def prt(lev, str):
+	global options
+	if options.verbose >= lev:
+		print (str)
+
+################################################################################
+# A simple class holding the table
+#
+class WikiTable():
+	
+	########################################################################
+	# The skeleton for the new additions
+	#
+	__skeleton = """\
+== Skeleton ==
+
+{| border="0" style="font-size: smaller; background-color: #eeeeee" bgcolor="#eeeeee"
+
+|- valign="top"
+|- bgcolor="#6699ff"
+|Example || Example version || Library version || Who || Date || Status || BMP || OOCD || OOCD Serial || STLINK || STFLASH
+
+<!-- BEGIN UPDATE -->
+
+<!-- END UPDATE -->
+|}
+
+"""
+
+	def __init__(self):
+		self.parse(self.__skeleton.splitlines(1))
+	
+	########################################################################
+	# Deserialize the table from the list of lines
+	#
+	def parse(self, lines):
+		self.header = []
+		self.footer = []
+		self.table = []
+		text = []
+		curpos = 0
+		for line in lines:
+			if line.find("END UPDATE") != -1:
+				curpos = 2
+			
+			if curpos == 0:
+				self.header.append(line)
+			elif curpos == 1:
+				text.append(line)
+			elif curpos == 2:
+				self.footer.append(line)
+			
+			if line.find("BEGIN UPDATE") != -1:
+				curpos = 1
+		
+		total = ""
+		for line in text:
+			ln = line.rstrip('\n')
+			if (len(ln) > 1):
+				total += ln
+			elif (len(total) > 0):
+				self.table.append(total.lstrip("|").split("|"))
+				total = ""
+		
+
+	########################################################################
+	# Serialize the table into the string
+	#
+	def serialize(self):
+		text = []
+		text.extend(self.header)
+		for tab in self.table:
+			for i in range(len(tab)):
+				if (len(tab[i]) != 0) and (i % 2 == 0) :
+					text.append("|" + tab[i] + "\n")
+				else:
+					text.append("|" + tab[i])
+			text.append("\n")
+		text.extend(self.footer)
+		return (text)
+	
+	########################################################################
+	# Append the missing directories
+	#
+	def do_append_rows(self,rows):
+		for dir in rows:
+			prt(1, "Example added '%s'" % (dir))
+			self.table.append(['-valign="top"', '', dir, '', '&mdash;',
+				'', '&mdash;', '', '&mdash;', '', '&mdash;',
+				'', '&mdash;'])
+
+	########################################################################
+	# Update color of each row to match its status
+	#
+	def do_update_colors(self):
+		for tab in self.table:
+			style = 'style="background:lime"'
+			if (tab[4].count(options.exver) == 0) \
+				or (tab[6].count(options.libver) == 0): 
+				style = 'style="background:yellow"'
+	
+			if (tab[8].count("&mdash;") > 0):
+				style = 'style="background:orange"'
+	
+			if tab[1].find(style) == -1:
+				prt(1, "Example state touched '%s'" % (tab[2].strip()))
+		
+			tab[1] = style;
+			tab[3] = style;
+			tab[5] = style;
+			tab[7] = style;
+			tab[9] = style;
+	
+
 def browse(path):
 	dirs = [ ]
 	for x in os.walk(path):
@@ -34,78 +149,78 @@ def browse(path):
 			dirs.append(dir)
 	return (dirs)
 
-def splithdrs(file):
-	(header, text, footer) = ([], [], [])
-	curpos = 0
-	
-	with open(file) as f:
-		for line in f:
-			if line.find("END UPDATE") != -1:
-				curpos = 2
-			
-			if curpos == 0:
-				header.append(line)
-			elif curpos == 1:
-				text.append(line)
-			elif curpos == 2:
-				footer.append(line)
-			
-			if line.find("BEGIN UPDATE") != -1:
-				curpos = 1
 
-	return (header, text, footer)
 
-def splittbl(text):
-	rows = []
-	total = ""
-	for line in text:
-		ln = line.rstrip('\n')
-		if (len(ln) > 1):
-			total += ln
-		elif (len(total) > 0):
-			rows.append([total.lstrip("|").split("|")])
-			total = ""
-	
-	return (rows)
-
-def row_put(f, value):
-	for i in range(len(value)):
-		f.write("|")
-		f.write(value[i])
-		if (len(value[i]) != 0) and (i % 2 == 0) :
-			f.write("\n")
-	f.write("\n")
-	
-def row_colorize(row):
+def write_file(wt):
 	global options
 	
-	style = 'style="background:lime"'
-	if (row[4].count(options.exver) == 0) or (row[6].count(options.libver) == 0): 
-		style = 'style="background:yellow"'
-
-	if (row[8].count("&mdash;") > 0):
-		style = 'style="background:orange"'
-
-	changed = row[1].find(style) == -1
+	text = wt.serialize()
 	
-	row[1] = style;
-	row[3] = style;
-	row[5] = style;
-	row[7] = style;
-	row[9] = style;
-	
-	return (changed)
+	if options.backup:
+		shutil.copy2(options.file, options.file+".bak")
+		prt(2, "Examples wiki file backupped to '%s'." % (options.file + ".bak"))
 
-def prt(lev, str):
-	global options
-	if options.verbose >= lev:
-		print (str)
+	with open(options.file, "w") as f:
+		f.writelines(text)
+	
+	prt(1, "Examples wiki file updated.")
+
 
 def git_ver(path):
 	return subprocess.check_output("git --git-dir=%s/.git rev-parse --short=10 HEAD" % (path), shell=False, universal_newlines=True).rstrip('\n')
 
 
+
+################################################################################
+# Commands 
+def cmd_init_tree():
+	global options
 	
+	wt = WikiTable()	
+	
+	dirs = browse(options.tree + "/examples/stm32/f2")
+	wt.do_append_rows(dirs)
+	
+	write_file(wt)
+
+def cmd_update_tree():
+	global options
+	
+	dirs = browse(options.tree + "/examples/stm32/f2")
+
+	with open(options.file) as f:
+		lines = f.readlines()
+	
+	wt = WikiTable()
+	wt.parse(lines)
+
+	# check all dirs involved
+	for tab in wt.table:
+		try:
+			dirs.remove(tab[2].strip())
+		except:
+			prt(1, "Example no longer exists '%s'" % (tab[2].strip()))
+	
+	# append nonexistent dirs
+	wt.do_append_rows(dirs)
+
+	write_file(wt)
+
+def cmd_update_gitver():
+	global options
+
+	with open(options.file) as f:
+		lines = f.readlines()
+	
+	wt = WikiTable()
+	wt.parse(lines)
+
+	# recolorize the table
+	wt.do_update_colors()
+
+	write_file(wt)
+
+
 def main():
 	
 	parser = OptionParser(
@@ -113,13 +228,15 @@ def main():
 		"Commands:\n"
 		"  pass\t\t Mark selected examples in the specified tree as passed\n"
 		"  fail\t\t Mark selected examples in the specified tree as failed\n"
-		"  treeupdate\t Update the examples tree\n"
-		"  gitupdate\t Update table to the actual git version\n\n"
+		"  init-tree\t Initialize tree\n"
+		"  update-tree\t Update the examples tree\n"
+		"  update-gitver\t Update table to the actual git version\n\n"
 		"Examples:\n"
 		" > edit.py -f<file> -t<tree> [-u<user> -l<libver> -e<exver>] pass ./brd/example\n"
 		" > edit.py -f<file> -t<tree> [-u<user> -l<libver> -e<exver>] fail ./brd/example\n"
-		" > edit.py -f<file> -t<tree> treeupdate\n"
-		" > edit.py -f<file> -t<tree> [-l<libver> -e<exver>]  gitupdate\n"
+		" > edit.py -f<file> -t<tree> init-tree\n"
+		" > edit.py -f<file> -t<tree> update-tree\n"
+		" > edit.py -f<file> -t<tree> [-l<libver> -e<exver>] update-gitver\n"
 		, version="%prog version 0.1")
 	
 	parser.add_option("-q", "--quiet", action="store_const", const=0, dest="verbose", default=1, help="Be very silent")
@@ -150,7 +267,7 @@ def main():
 
 	if (options.file == None):
 		#parser.error("git: Missing required arguments (-f)")
-		options.file = "../wiki/stm32f1.wiki"
+		options.file = "../wiki/stm32f2.wiki"
 	
 	if (options.tree == None):
 		options.tree = os.environ.get('LOCM3_EXAMPLES')
@@ -166,20 +283,7 @@ def main():
 		options.user = os.getlogin()
 		prt(2, "Using login of current logged user '%s' as username" % (options.user))
 
-	needver = False
-	if args[0] == "gitupdate":
-		pass
-		needver = 1
-	elif args[0] == "treeupdate":
-		pass
-	elif args[0] == "pass":
-		pass
-		needver = 1
-	elif args[0] == "fail":
-		pass
-		needver = 1
-	else:
-		parser.error("Unknown command: '%s'" % (args[0]))
+	needver = (args[0] == "update-gitver") or (args[0] == "pass") or (args[0] == "fail")
 	
 	if needver and (options.exver == None):
 		options.exver = git_ver(options.tree)
@@ -192,41 +296,20 @@ def main():
 	
 	
 	# The main body
-	dirs = browse(options.tree + "/examples/stm32/f1")
-
-	(header, text, footer) = splithdrs(options.file)
-	table = splittbl(text)
-
-	# check all dirs involved
-	for tab in table:
-		try:
-			dirs.remove(tab[0][2].strip())
-		except:
-			prt(1, "Example no longer exists '%s'" % (tab[0][2].strip()))
 	
-	# append nonexistent dirs
-	for dir in dirs:
-		prt(1, "Example added '%s'" % (dir))
-		table.append([['-valign="top"', '', dir, '', '&mdash;', '', '&mdash;', '', '&mdash;', '', '&mdash;', '', '&mdash;', '', '&mdash;', '', '&mdash;', '', '&mdash;', '', '&mdash;', '', '&mdash;']])
+	if args[0] == "init-tree":
+		cmd_init_tree()
+	elif args[0] == "update-tree":
+		cmd_update_tree()
+	elif args[0] == "update-gitver":
+		cmd_update_gitver()
+	elif args[0] == "pass":
+		pass
+	elif args[0] == "fail":
+		pass
+	else:
+		parser.error("Unknown command: '%s'" % (args[0]))
 
-	# recolorize the table
-	for tab in table:
-		if row_colorize(tab[0]):
-			prt(1, "Example state touched '%s'" % (tab[0][2].strip()))
-
-	if options.backup:
-		shutil.copy2(options.file, options.file+".bak")
-		prt(2, "Examples wiki file backupped to '%s'." % (options.file + ".bak"))
-	
-	with open(options.file, "w") as f:
-		for line in header:
-			f.write(line)
-		for tab in table:
-			row_put(f, tab[0])
-		for line in footer:
-			f.write(line)
-	
-	prt(1, "Examples wiki file updated.")
 
 
 if __name__ == "__main__":
